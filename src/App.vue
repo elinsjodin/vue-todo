871e108dce8480b923465fe24f174d556eecf16c
elinsjodin <elinsjodin@outlook.com>
2022-05-10 11:09:26 +0200

    Implemented animation for loading component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,43 @@
 <template>
-  <ShowTodoList></ShowTodoList>
+  <Loader
+    v-if="loadingTodos"
+    loadingWrapper="loading-wrapper"
+    loadingContainer="loading-container"
+    lines="lines"
+    diamond="diamond"
+  ></Loader>
+  <div v-else>
+    <ShowTodoList></ShowTodoList>
+  </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import Loader from "./components/Loader.vue";
 import ShowTodoList from "./components/ShowTodoList.vue";
-import ShowTodos from "./components/ShowTodos.vue";
 
 @Options({
   components: {
     ShowTodoList,
-    ShowTodos,
     Loader,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  loadingTodos = true;
+
+  mounted() {
+    setTimeout(() => {
+      this.loadingTodos = !this.loadingTodos;
+    }, 1200);
+  }
+}
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+// body {
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+// }
+</style>
--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,11 +1,94 @@
 <template>
-  <div>Todo List is loading...</div>
+  <div :class="loadingWrapper">
+    <div :class="loadingContainer">
+      <div :class="lines"></div>
+      <div :class="diamond"></div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Vue } from "vue-class-component";
+import { Prop } from "vue-property-decorator";
 
-export default class Loader extends Vue {}
+export default class Loader extends Vue {
+  @Prop() loadingWrapper!: string;
+  @Prop() loadingContainer!: string;
+  @Prop() lines!: string;
+  @Prop() diamond!: string;
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.loading-wrapper {
+  min-width: none;
+  max-width: 500px;
+  .loading-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .lines {
+      min-width: 90%;
+      height: 10px;
+      position: relative;
+      // @include tablet {
+      //   min-width: 110%;
+      // }
+      // @include desktop {
+      //   height: 14px;
+      //   margin-bottom: 14px;
+      // }
+    }
+    .lines::after,
+    .lines::before {
+      content: "";
+      position: absolute;
+      margin: auto;
+      height: 1.8px;
+      background-color: rgb(199, 134, 134);
+      width: 46%;
+      top: 195%;
+      animation: line 1s ease-in;
+      // @include tablet {
+      //   height: 2px;
+      // }
+    }
+    .lines::after {
+      left: 0;
+    }
+    .lines::before {
+      right: 0;
+    }
+    .diamond {
+      height: 20px;
+      width: 20px;
+      border: 2px solid rgb(180, 167, 93);
+      margin: auto;
+      transform: rotate(45deg);
+      // @include tablet {
+      //   height: 6px;
+      //   width: 6px;
+      //   border: 2px solid $black;
+      // }
+      // @include desktop {
+      //   height: 10px;
+      //   width: 10px;
+      // }
+    }
+    @keyframes line {
+      0% {
+        transform: translateX(-120%);
+        opacity: 0;
+      }
+      50% {
+        transform: translateX(0%);
+        opacity: 1;
+      }
+      0% {
+        left: 53%;
+      }
+    }
+  }
+}
+</style>
